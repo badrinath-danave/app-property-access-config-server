340ae006c3e788a5e1c59f1217d12de3572d022f
Badrinath Danave <badrinathdanave@gmail.com>
2023-03-24 23:21:37 +0530

    intial commit

---
+++ src/main/java/com/bdanave/microservices/AppPropertyAccessConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bdanave.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPropertyAccessConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPropertyAccessConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdanave/microservices/AppPropertyAccessConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdanave.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPropertyAccessConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
